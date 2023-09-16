d24830ccecf94b7d78f59bc24913bcf49f5a3ad7
Lori <lorigirton@outlook.com>
2023-09-16 14:54:13 -0400

    Fixed schema

--- Crowdfunding(2).sql
+++ Crowdfunding(2).sql
@@ -1,15 +1,15 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/sRfu9A
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "campaign" (
-    "cf_id" INTEGER,   NOT NULL,
+    "cf_id" INTEGER   NOT NULL,
     "contact_id" INTEGER   NOT NULL,
     "company_name" VARCHAR(100)   NOT NULL,
     "description" TEXT   NOT NULL,
-    "goal" INTEGER   NOT NULL,
-    "pledged" INTEGER   NOT NULL,
+    "goal" REAL   NOT NULL,
+    "pledged" REAL   NOT NULL,
     "outcome" VARCHAR(10)   NOT NULL,
     "backers_count" INTEGER   NOT NULL,
     "country" VARCHAR(10)   NOT NULL,
@@ -25,19 +25,28 @@ CREATE TABLE "campaign" (
 
 CREATE TABLE "category" (
     "category_id" VARCHAR(5)   NOT NULL,
-    "category" VARCHAR(15)   NOT NULL
+    "category" VARCHAR(15)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
 );
 
 CREATE TABLE "subcategory" (
     "subcategory_id" VARCHAR(10)   NOT NULL,
-    "subcategory" VARCHAR(25)   NOT NULL
+    "subcategory" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
 );
 
 CREATE TABLE "contacts" (
     "contact_id" INTEGER   NOT NULL,
     "first_name" VARCHAR(30)   NOT NULL,
     "last_name" VARCHAR(30)   NOT NULL,
-    "email" VARCHAR(50)   NOT NULL
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
 );
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
